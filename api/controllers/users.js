0f27a9e8f4d54d53c0dc08b77973e3c20575b15b
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-12-27 16:04:00 +0800

    Add email confirmation upon user registration

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-Users = mongoose.model('Users');
+var mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+var Users = mongoose.model('Users');
 
 exports.all = function(req, res) {
   Users.find({}, function(err, users) {
@@ -16,7 +17,42 @@ exports.add = function(req, res) {
   _new.save(function(err, users) {
     if (err) res.send(err);
 
-    // ADD EMAIL CONFIRMATION BLOCK HERE
+    /**
+    * email block goes here
+    */
+
+    nodemailer.createTestAccount((err, account) => {
+
+      // create reusable transporter object using the default SMTP transport
+      const transporter = nodemailer.createTransport({
+        host: 'mail.smtp2go.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS
+        }
+      });
+
+      // setup email data with unicode symbols
+      let mailOptions = {
+        from: 'lorenzosalamante@gmail.com', // sender address
+        to: 'lorenzosalamante@gmail.com', // list of receivers
+        subject: 'Activate your account ENV', // Subject line
+        html: '<a href="http://localhost:3000/users/activate/?code=' + users.activationCode + '">Activate your account</a>' // html body
+      };
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+    });
+
+    /**
+    * email block goes here
+    */
 
     res.json(users);
   });
@@ -47,10 +83,10 @@ exports.update = function(req, res) {
 
 
 /**
- * format the request body to appropriate format
- * @param  {obj} unformatted raw req.body
- * @return {obj}             [description]
- */
+* format the request body to appropriate format
+* @param  {obj} unformatted raw req.body
+* @return {obj}             [description]
+*/
 var formatFields = function(unformatted){
   var formatted = {}
   formatted.name = {

eacadcb47fc9917b59452d8e8a59c533489acc75
Lorenzo Salamante <lsalamante@myoptimind.com>
2018-01-13 14:34:31 +0800

    Refactor file and change JWT structure

--- models/Users.js
+++ models/Users.js
@@ -1,10 +1,10 @@
-'use strict';
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const crypto = require('crypto');
-const bcrypt = require('bcrypt-nodejs');
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
+'use strict'
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const crypto = require('crypto')
+const bcrypt = require('bcrypt-nodejs')
+const nodemailer = require('nodemailer')
+const jwt = require('jsonwebtoken')
 
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
@@ -15,11 +15,11 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   }
-});
+})
 
 const schema = mongoose.Schema(
   {
-    name:  {
+    name: {
       type: String,
       trim: true,
       required: true
@@ -41,11 +41,11 @@ const schema = mongoose.Schema(
       unique: true,
       sparse: true, // @link: https://stackoverflow.com/questions/24430220/e11000-duplicate-key-error-index-in-mongodb-mongoose
       validate: {
-        validator: function(v) {
-          return /^[a-zA-Z0-9_]*$/.test(v); // @link: https://stackoverflow.com/a/336220/7800523
+        validator: function (v) {
+          return /^[a-zA-Z0-9_]*$/.test(v) // @link: https://stackoverflow.com/a/336220/7800523
         },
         message: 'The username field only allows alphanumeric characters and undescores'
-      },
+      }
     },
     addresses: [{ label: String, coord: String }],
     avatar: String, // TODO: Gravatar
@@ -59,39 +59,45 @@ const schema = mongoose.Schema(
       default: 'pending'
     },
     activationCode: {
-      type: String,
-    },
+      type: String
+    }
   },
   {
     timestamps: true
   }
-);
+)
 
-schema.statics.hashPassword = function(password) { // @link: https://stackoverflow.com/questions/29664499/mongoose-static-methods-vs-instance-methods
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
-};
+schema.statics.hashPassword = function (password) { // @link: https://stackoverflow.com/questions/29664499/mongoose-static-methods-vs-instance-methods
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+}
 
-schema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
-};
+schema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.password)
+}
 
-schema.methods.generateActivationCode = function(code) {
-  return crypto.randomBytes(30).toString('hex');
-};
+schema.methods.generateActivationCode = function (code) {
+  return crypto.randomBytes(30).toString('hex')
+}
 
-schema.methods.generateJWT = function() {
-  return jwt.sign({id: this._id}, process.env.SECRET_OR_KEY); //REVIEW: JWT Standards
-};
+schema.methods.generateJWT = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      iat: Date.now()
+      // exp: Math.floor(Date.now() / 1000) + (60 * 60) // Make expiration 1 hour TODO: Add expiration to tokens
+    },
+  process.env.SECRET_OR_KEY) // REVIEW: JWT Standards
+}
 
-schema.methods.sendActivationCode = function() {
+schema.methods.sendActivationCode = function () {
   let mailOptions = { // setup email data with unicode symbols
     from: process.env.FROM_EMAIL, // sender address FIXME
     to: this.email, // list of receivers
     subject: 'Activate your account', // Subject line
-    html: '<a href="http://localhost:3000/v1/users/activate/?code=' + this.activationCode + '">Activate your account</a>' // html body FIXME
-  };
-  transporter.sendMail(mailOptions, (error, info) => { if (error) { console.log(error); } }); // send mail with defined transport object
-};
+    html: '<a href="http://localhost:3000/v1/users/activate/?code=' + this.activationCode + '">Activate your account</a>' // html body FIXME: Base url
+  }
+  transporter.sendMail(mailOptions, (error, info) => { if (error) { console.log(error) } }) // send mail with defined transport object
+}
 
-schema.plugin(uniqueValidator);
-mongoose.model('Users', schema);
+schema.plugin(uniqueValidator)
+mongoose.model('Users', schema)
